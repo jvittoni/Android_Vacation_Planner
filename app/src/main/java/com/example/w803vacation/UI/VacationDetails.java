862c5fd7bea4e5e09f2e6b182e218d124b00cd7d
jvittoni <javaevittoni@gmail.com>
2024-12-14 21:08:19 -0800

    Webinar 1 Complete

---
+++ app/src/main/java/com/example/w803vacation/UI/VacationDetails.java
@@ -0,0 +1,32 @@
+package com.example.w803vacation.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.w803vacation.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class VacationDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_vacation_details);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(VacationDetails.this, ExcursionDetails.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
